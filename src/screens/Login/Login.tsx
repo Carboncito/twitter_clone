112108b034f365cf06b36c4b42380e33488de228
Carboncito <carboncito760@gmail.com>
2023-04-11 22:09:33 -0600

    remove unnecessary code

--- src/screens/Login/Login.tsx
+++ src/screens/Login/Login.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
-import { Image, View } from 'react-native';
+import { View } from 'react-native';
 import { Text } from 'components';
-import { TwitterLogo, images } from 'assets';
+import { TwitterLogo } from 'assets';
 
 const Login = () => {
   return (
     <View>
       <TwitterLogo />
       <Text h1>See what's happening in the world right now.</Text>
-      <Image source={images.danny_fenton} />
     </View>
   );
 };
