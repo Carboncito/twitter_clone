da3a7b1a74816f106ad5876b067d3166d09af382
Carboncito <carboncito760@gmail.com>
2023-05-23 16:17:41 -0600

    remove storybook

--- App.tsx
+++ App.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import { Navigator } from './src/router';
 import { ThemeProvider } from './src/theme';
 
-const STORYBOOK_START = true;
-
 const App = (): JSX.Element => {
   return (
     <ThemeProvider>
@@ -12,11 +10,4 @@ const App = (): JSX.Element => {
   );
 };
 
-let AppEntryPoint = App;
-
-// Render Storybook if storybookEnabled is true
-if (STORYBOOK_START) {
-  AppEntryPoint = require('./.storybook').default;
-}
-
-export default AppEntryPoint;
+export default App;
